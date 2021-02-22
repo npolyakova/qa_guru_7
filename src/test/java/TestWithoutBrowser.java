51329183d7d3951f06b070a20405cb5d0e6cc881
Natalya <n.polyakova.kazanexpress@gmail.com>
2021-02-22 14:46:01 +0300

    added tests and gradle

---
+++ src/test/java/TestWithoutBrowser.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestWithoutBrowser {
+
+    int sum = 2 + 2;
+
+    @Test
+    public void successOne(){
+        assertEquals(sum,4);
+    }
+
+    @Test
+    public void negativeOne(){
+        assertEquals(sum,2);
+    }
+}
